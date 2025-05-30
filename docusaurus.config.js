20d18fe30be13dde2032b4a548932974555ade56
Lucas <41251864+LucasLiu09@users.noreply.github.com>
2025-05-30 22:53:57 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,6 +74,10 @@ const config = {
     ],
   ],
 
+  plugins: [
+    'plugin-image-zoom'
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -162,6 +166,19 @@ const config = {
         indexName: 'lucasliu09io',
         // Optional: see doc section below
         contextualSearch: true,
+      },
+      imageZoom: {
+        // CSS selector to apply the plugin to, defaults to '.markdown img'
+        selector: '.markdown img',
+        // Optional medium-zoom options
+        // see: https://www.npmjs.com/package/medium-zoom#options
+        options: {
+          margin: 24,
+          background: '#BADA55',
+          scrollOffset: 0,
+          container: '#zoom-container',
+          template: '#zoom-template',
+        },
       },
     }),
 };
