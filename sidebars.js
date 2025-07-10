9c001b5ad0ebf5ac3b67ebf5104b79cd6827edc9
Lucas <41251864+LucasLiu09@users.noreply.github.com>
2025-07-10 10:05:10 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -61,6 +61,7 @@ const sidebars = {
           items: [
             'odoo/dev_notes/models/model_email_template',
             'odoo/dev_notes/models/constraints',
+            'odoo/dev_notes/models/create_database_view',
           ]
         },
         {
