434019ef6ffb4288a286e9ba536c6fda27bacc29
Lucas <41251864+LucasLiu09@users.noreply.github.com>
2025-09-15 14:44:11 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,12 +18,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
+            className="button button--primary button--lg"
             to="/docs/odoo/">
             Odoo Documents
           </Link>
