5bb2286882349fa61177f0f06ccf4e422a4cec66
Lucas <41251864+LucasLiu09@users.noreply.github.com>
2025-09-15 14:41:25 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ function HomepageHeader() {
           </Link>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/odoo/index">
+            to="/docs/odoo/">
             Odoo Documents
           </Link>
         </div>
