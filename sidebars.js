ac0216a2b4dd2f5defb1cc1b10d7efd132cf7ab8
Lucas <41251864+LucasLiu09@users.noreply.github.com>
2026-02-05 15:18:16 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -151,6 +151,7 @@ const sidebars = {
                 'odoo/dev_notes/front-end/fields/float_field_extend',
                 'odoo/dev_notes/front-end/fields/char_field_extension',
                 'odoo/dev_notes/front-end/fields/boolean_field_extension',
+                'odoo/dev_notes/front-end/fields/selection_field_extension',
                 'odoo/dev_notes/front-end/fields/binary_field_extension',
                 'odoo/dev_notes/front-end/fields/x2many_field_extend',
                 'odoo/dev_notes/front-end/fields/many2many_field_extension',
